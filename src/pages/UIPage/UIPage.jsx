ad3b799a6f2d55183654eb2f1b0e5ddad5e4473b
saals <salekser@gmail.com>
2023-03-04 20:46:54 +0300

    app setting

---
+++ src/pages/UIPage/UIPage.jsx
@@ -0,0 +1,14 @@
+import styles from './ui-page.module.scss'
+
+const UIPage = () => {
+  return (
+    <div className={styles.column}>
+      <h1>h1h1h1</h1>
+      <h2>h2h2h2</h2>
+      <h3>h3h3h3</h3>
+      <button>bbb</button>
+    </div>
+  )
+}
+
+export default UIPage
